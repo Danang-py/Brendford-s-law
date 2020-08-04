83934a3c65ebbab45bd59e6679a4238629f4351e
pouletmortuaire <raphaelhebert18@gmail.com>
2020-08-04 10:17:22 +0700

    First commit, let's code that!

---
+++ brendford.py
@@ -0,0 +1,7 @@
+For a given dataset, - Get, or make, the data set to examine
+
+- Find the percent of numbers that start with 1...
+... - Then the percent of numbers that start with 2...
+.... - and so forth, until you have all 9 percentages
+
+- See if the percentages are close (or match) Benford's law.... if not, then there is something fishy here 
